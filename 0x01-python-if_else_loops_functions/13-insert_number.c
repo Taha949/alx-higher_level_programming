fdec0ee6272a385a357ef31ef23ac9aa6bd801cd
taha949 <maatataha@gmail.com>
2023-06-06 17:39:57 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,8 +3,7 @@
 /**
  * insert_node - Inserts a number into a sorted singly-linked list.
  * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- * Return: NULL.
+ * Return: null
  *         Otherwise - a pointer to the new node.
  */
 listint_t *insert_node(listint_t **head, int number)
