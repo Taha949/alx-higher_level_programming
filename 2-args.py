2dff8a083156c59cd9369f110c606c3c3b5c557c
taha949 <maatataha@gmail.com>
2023-06-08 07:53:37 -0700

    2-args.py

--- 2-args.py
+++ 2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
+    
     import sys
 
-    c = len(sys.argv) - 1
-    if c == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif c == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(c))
-    for i in range(c):
+        print("{} arguments:".format(count))
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
