b1d0599dcb0b6cf69552ef64274ce5547675f687
taha949 <maatataha@gmail.com>
2023-06-08 07:39:44 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if len(sys.argv)-1 == 0:
         print("0 arguments.")
     else:
         print(f"{len(sys.argv) - 1} arguments:")
-        for i in range(1, len(sys.argv)):
-            print(f"{i}: {sys.argv[i]}")
+        for i in range(1, len(sys.argv-1)):
+            print(f"{i+1}: {sys.argv[i+1]}")
